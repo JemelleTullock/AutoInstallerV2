7e3aeb8dbb04d3f69376cf1ed064ca37bc2a6b1c
JemelleTullock <121142104+JemelleTullock@users.noreply.github.com>
2023-03-12 17:22:18 -0700

    Update AutoInstallerV2.py

--- AutoInstallerV2.py
+++ AutoInstallerV2.py
@@ -2,9 +2,6 @@ import os
 import time
 
 os.system("title AutoInstallerV2")
-
-import os
-
 os.system("color 0C")
 
 app_dirs = input("Please enter the paths to the directories containing the applications (separated by commas): ").split(",")
@@ -22,6 +19,28 @@ for app_dir in app_dirs:
             os.system(filepath)
         else:
             print(f"\033[31mSkipping {filename} (not an executable file)\033[0m")
+
+while True:
+    answer = input("Are you done with the installation process? (y/n): ").lower()
+    if answer == "y":
+        break
+    elif answer == "n":
+        app_dirs = input("Please enter the paths to the directories containing the additional applications (separated by commas): ").split(",")
+        for app_dir in app_dirs:
+            app_dir = app_dir.strip()
+            if not os.path.exists(app_dir):
+                print(f"\033[31mError:\033[0m Directory '{app_dir}' does not exist.")
+                continue
+                
+            for filename in os.listdir(app_dir):
+                filepath = os.path.join(app_dir, filename)
+                if os.path.isfile(filepath) and filename.endswith(".exe"):
+                    print(f"\033[31mInstalling {filename}...\033[0m")
+                    os.system(filepath)
+                else:
+                    print(f"\033[31mSkipping {filename} (not an executable file)\033[0m")
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
 
 countdown_time = 5
 for i in range(countdown_time, 0, -1):
